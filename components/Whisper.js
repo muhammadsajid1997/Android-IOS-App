ad89483c9753479c2c38bff7e48fd4a1be7a33c9
dhaval-cy <89638808+dhaval-cy@users.noreply.github.com>
2023-04-03 18:28:32 +0530

    menu section UI Changes

--- components/Whisper.js
+++ components/Whisper.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {Platform, StyleSheet, Text, Button, View, ActivityIndicator, Image, TouchableOpacity, ScrollView, Modal, Alert, Pressable } from 'react-native';
+import { Platform, StyleSheet, Text, Button, View, ActivityIndicator, TextInput, Image, TouchableOpacity, ScrollView, Modal, Alert, Pressable, BackHandler } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import Voice from '@react-native-voice/voice';
 import axios from "axios";
@@ -15,8 +15,9 @@ import { signOutFunc, auth } from "../firebaseConfig";
 import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 import * as Location from 'expo-location';
+// import { TextInput } from 'react-native-paper';
 
-export default function App({  }) {
+export default function App({ }) {
   let [started, setStarted] = useState(false);
   let [results, setResults] = useState("");
   let [voiceResult, setVoiceResult] = useState("");
@@ -25,6 +26,7 @@ export default function App({  }) {
   const [typeText, setTypeText] = useState("");
   const [receivedText, setReceivedText] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
+  const [modalSection, setSection] = useState("Home");
   const soundRef = useRef(null);
   const navigation = useNavigation();
 
@@ -41,8 +43,23 @@ export default function App({  }) {
 
 
   useEffect(() => {
+    const backAction = () => {
+      setModalVisible(false),
+        setSection("Home")
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  useEffect(() => {
     (async () => {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
@@ -50,7 +67,7 @@ export default function App({  }) {
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      // console.log(location)
+      console.log(location)
       setLocation(location);
     })();
   }, []);
@@ -174,101 +191,284 @@ export default function App({  }) {
   //   }
   // }
 
-  return (
 
-    <View style={styles.root}>
+  const RenderSections = () => {
+    if (modalSection == 'Home') {
+      return <View style={styles.root}>
+        {!showInput ?
+          <>
+            <View style={{ alignSelf: 'flex-end', margin: 30, backgroundColor: 'black', padding: 1 }}>
+              <Text style={{ color: 'white' }}> CC </Text>
+            </View>
 
-      {!showInput ?
-        <>
-          <View style={{ alignSelf: 'flex-end', margin: 30, backgroundColor: 'black', padding: 1 }}>
-            <Text style={{ color: 'white' }}> CC </Text>
-          </View>
-
-          <View style={{ flex: 1, marginHorizontal: 20, alignItems: 'center' }}>
-            <View>
-              {!results ?
-                <>
-                  <Text style={styles.title}>Start Speaking </Text>
-                  <Text>{text}</Text>
+            <View style={{ flex: 1, marginHorizontal: 20, alignItems: 'center' }}>
+              <View>
+                {!results ?
+                  <>
+                    <Text style={styles.title}>Start Speaking </Text>
+                    {/* <Text>{text}</Text> */}
 
 
 
-                  <Text style={styles.title}> To Activate Alli </Text>
-                </>
+                    <Text style={styles.title}> To Activate Alli </Text>
+                  </>
+                  :
+                  <ScrollView ref={scrollViewRef} style={styles.scrollViewText}>
+                    <TextEffect text={results} />
+                  </ScrollView>
+
+                }
+
+              </View>
+            </View>
+
+            <View style={styles.settingsSection}>
+              <Image style={{ borderRadius: 80 }} source={Logo} />
+            </View>
+
+            <View style={styles.container}>
+
+              {!isLoading ?
+                <TouchableOpacity style={[styles.circleButton, { backgroundColor: !started ? '#fff' : "#000" },]}
+                  onPress={!started ? startSpeechToText : stopSpeechToText}>
+                  <FontAwesome name="microphone" size={32} color={!started ? "#000" : "#fff"} />
+                </TouchableOpacity>
                 :
-                <ScrollView ref={scrollViewRef} style={styles.scrollViewText}>
-                  <TextEffect text={results} />
-                </ScrollView>
-
+                <TouchableOpacity style={[styles.circleButton, { backgroundColor: "#000" }]} disabled>
+                  <ActivityIndicator size={32} color="#fff" />
+                </TouchableOpacity>
               }
 
             </View>
-          </View>
-
-          <View style={styles.settingsSection}>
-            <Image style={{ borderRadius: 80 }} source={Logo} />
-          </View>
-
-          <View style={styles.container}>
-
-            {!isLoading ?
-              <TouchableOpacity style={[styles.circleButton, { backgroundColor: !started ? '#fff' : "#000" },]}
-                onPress={!started ? startSpeechToText : stopSpeechToText}>
-                <FontAwesome name="microphone" size={32} color={!started ? "#000" : "#fff"} />
-              </TouchableOpacity>
+          </>
+          :
+          <>
+            <View style={{ marginTop: 10, width: "100%" }}>
+              <Text style={{ ...styles.title, fontWeight: "700", fontSize: 35, color: "#0f87cf", marginVertical: 10, }}>Hey Alli </Text>
+              <CustomInput onSend={handleSend} isLoading={isLoading} />
+            </View>
+            <View style={{ marginTop: 10, width: "100%" }}>
+              <ChatUI TypeText={typeText} ReceivedText={receivedText} />
+            </View>
+          </>
+        }
+        <View style={styles.bottomBar}>
+          <TouchableOpacity onPress={handleInputField}>
+            {!showInput ?
+              <AntDesign name="form" size={24} color="black" />
               :
-              <TouchableOpacity style={[styles.circleButton, { backgroundColor: "#000" }]} disabled>
-                <ActivityIndicator size={32} color="#fff" />
-              </TouchableOpacity>
-            }
+              <AntDesign name="enter" size={24} color="black" />}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <Entypo name="menu" size={28} color={"#000"} />
+          </TouchableOpacity>
 
-          </View>
-        </>
-        :
-        <>
-          <View style={{ marginTop: 10, width: "100%" }}>
-            <Text style={{ ...styles.title, fontWeight: "700", fontSize: 35, color: "#0f87cf", marginVertical: 10, }}>Hey Alli </Text>
-            <CustomInput onSend={handleSend} isLoading={isLoading} />
-          </View>
-          <View style={{ marginTop: 10, width: "100%" }}>
-            <ChatUI TypeText={typeText} ReceivedText={receivedText} />
-          </View>
-        </>
-      }
-      <View style={styles.bottomBar}>
-        <TouchableOpacity onPress={handleInputField}>
-          {!showInput ?
-            <AntDesign name="form" size={24} color="black" />
-            :
-            <AntDesign name="enter" size={24} color="black" />}
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-        <Entypo name="menu" size={28} color={"#000"} />
-  </TouchableOpacity> 
-       
-      </View>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
         </View>
-      </Modal>
 
-    </View>
+        <View style={styles.centeredView}>
+          <Modal
+
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={{ flex: 1 }}>
+              <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <View style={{ marginTop: 10, width: "100%" }}>
+                      <Text style={{ ...styles.title, fontWeight: "700", fontSize: 35, color: "#0f87cf", marginVertical: 10, }}>Hey Alli </Text>
+                      {/* <CustomInput onSend={handleSend} isLoading={isLoading} /> */}
+                    </View>
+
+                    <View style={{ padding: 10 }}>
+
+
+                      <View style={{ width: '85%', paddingVertical: 5, }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Profile</Text>
+                      </View>
+                      {/* <View style={{ flexDirection: 'row', width: '100%' }}>
+
+
+
+
+
+                        <View style={{ padding: 5 }}>
+                          <TextInput
+                            style={{
+                              flex: 0.9,
+                              height: 40,
+                            }}
+                            placeholder="Type your message here..."
+
+
+                            blurOnSubmit={false}
+                            returnKeyType="send"
+                          />
+
+                        </View> */}
+
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="First name"
+                          mode="outlined"
+                          style={{
+                            width: 150,
+                            // paddingHorizontal: 5,
+                            fontSize: 15,
+                          }}
+                        />
+                        <View style={{ padding: 5 }}>
+                          <TextInput
+                            label="last name"
+                            mode="outlined"
+                            style={{
+                              width: 150,
+                              // paddingHorizontal: 5,
+                              fontSize: 15,
+                            }}
+                          />
+
+                        </View>
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="Mobile"
+                          mode="outlined"
+                        />
+
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="email"
+                          mode="outlined"
+                        />
+
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="Age"
+                          mode="outlined"
+                        />
+
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="Gender"
+                          mode="outlined"
+                        />
+
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="Height"
+                          mode="outlined"
+                        />
+
+                      </View>
+                      <View style={{ padding: 5 }}>
+                        <TextInput
+                          label="Weight"
+                          mode="outlined"
+                        />
+
+                      </View>
+
+                      {/* <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 10 }}> */}
+
+
+                      {/* <TouchableOpacity style={{ width: '85%', borderWidth: 1, paddingVertical: 10, alignItems: 'center', borderRadius: 50 }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text>Submit</Text>
+                      </TouchableOpacity> */}
+
+                      {/* </View> */}
+
+
+                      <View style={{ width: '100%', paddingVertical: 5, marginTop: 10, }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Settings</Text>
+                        <View style={{ alignItems: 'center', justifyContent: 'center', height: 50, width: "100%", marginTop: 10 }}>
+                          <Text>Settings Sections</Text>
+                        </View>
+                      </View>
+
+                      <View style={{ width: '85%', paddingVertical: 5, marginTop: 10 }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Connected Apps</Text>
+                        <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
+
+
+
+                          <Image
+                            style={{
+                              width: 100,
+                              height: 100,
+
+                            }}
+                            source={{
+                              uri: 'https://reactnative.dev/img/tiny_logo.png',
+                            }}
+                          />
+                        </View>
+                      </View>
+
+
+
+
+
+                      <View style={{ width: '100%', paddingVertical: 5, marginTop: 10, }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Secret code</Text>
+                        <View style={{ alignItems: 'center', justifyContent: 'center', height: 50, width: "100%", marginTop: 10 }}>
+                          <Text>Secret code Sections</Text>
+                        </View>
+                      </View>
+
+                      <View style={{ width: '100%', paddingVertical: 5, marginTop: 10, }} onPress={() => { setModalVisible(false), setSection("Profile") }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Logout</Text>
+                        {/* <View style={{ alignItems: 'center', justifyContent: 'center', height: 50, width: "100%", marginTop: 10 }}>
+                        <Text>Secret code Sections</Text>
+                      </View> */}
+                      </View>
+
+
+
+
+
+                      {/* <TouchableOpacity style={{ width: '85%', borderWidth: 1, paddingVertical: 10, alignItems: 'center', borderRadius: 50, marginTop: 10 }} onPress={() => { setModalVisible(false), setSection("ConnectedApps") }}>
+                      <Text>Secret code</Text>
+                    </TouchableOpacity> */}
+                      {/* <TouchableOpacity style={{ width: '85%', borderWidth: 1, paddingVertical: 10, alignItems: 'center', borderRadius: 50, marginTop: 10 }} onPress={() => { setModalVisible(false), setSection("ConnectedApps") }}>
+                      <Text>Logout</Text>
+                    </TouchableOpacity> */}
+                    </View>
+
+                    {/* <Text style={styles.modalText}>Hello World!</Text>    
+              <Text style={styles.modalText}>Hello World!</Text>     */}
+
+
+
+
+                  </View>
+                </View>
+              </ScrollView>
+            </View>
+          </Modal>
+
+        </View>
+
+      </View>
+    }
+
+  }
+
+  return (
+
+
+    RenderSections()
+
+
   )
 }
 
@@ -379,22 +579,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 22,
+    height: "100%",
+    // marginTop: 22,
+    backgroundColor: '#fff'
   },
   modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    flex: 1,
+    borderWidth: 1,
+    width: "100%",
+    // height: 800,
   },
   button: {
     borderRadius: 20,
@@ -415,5 +608,7 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
   },
 });
